263efde2cdacc53b599eada8d57a4e81d8ab1a5b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-05-12 09:54:30 +0800

    Update Layout.js

--- site/src/components/Layout.js
+++ site/src/components/Layout.js
@@ -9,7 +9,7 @@ const meta = tags =>
   )
 
 export default ({
-  title = 'Summer of Making by Hack Club',
+  title = 'Summer of Making (a Hack Club Community Initiative)',
   desc = 'Let\'s make something awesome this summer! Hack Club\'s Summer of Making empowers you to make that passion project a reality. No more putting it off, now is the time!',
   url = 'https://summer.hackclub.com',
   path = '/',
