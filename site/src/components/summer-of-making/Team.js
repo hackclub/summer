9b47f51c52fd896fbe98692ae87461dd62d5fdd2
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-05-13 23:21:10 +0800

    Update Team.js

--- site/src/components/summer-of-making/Team.js
+++ site/src/components/summer-of-making/Team.js
@@ -39,7 +39,7 @@ const Module = ({ icon, name, body, ...props }) => (
     <Avatar
       size={64}
       src="https://hackclub.com/team/zach.jpg"
-      marginRight={2}
+      style={{ marginRight: '2'}}
     />
     <Box>
       <Heading.h3 color="snow" fontSize={3} mb={1} children={name} />
