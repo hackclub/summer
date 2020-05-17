9af778831c24b0ba98a2e9b2e0114e03838f5745
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-05-17 19:30:46 +0800

    Update Timeline.js

--- site/src/components/summer-of-making/Timeline.js
+++ site/src/components/summer-of-making/Timeline.js
@@ -112,7 +112,7 @@ export default () => (
       <Timeline.Step icon="send" name="Application Period" duration="Now to June 5th" />
       <Timeline.Step icon="message-new" name="Results Released" duration="June 10th"/>
       <Timeline.Step icon="bolt" name="Get Making!" duration="July 6th" />
-      <Timeline.Step icon="share" name="Project Showcase" duration="Aug. 14th" />
+      <Timeline.Step icon="share" name="Project Showcase" duration="Aug. 16th" />
     </Timeline>
   </Box.section>
 )
